f6145e2c6e5e74f16bc31c0b08fc7e49b87945b2
Dominik Krzeminski <raymon92@gmail.com>
2020-09-18 12:35:24 +0100

    making nature network ready2

--- experiments/nature_nn.py
+++ experiments/nature_nn.py
@@ -25,12 +25,14 @@ train_labels = pd.read_csv(DATA_DIR + 'train_labels.csv', index_col='sequence_id
 max_seq_length = 8000
 cnn_filter_length = 48
 
+print('Preprocessing start...')
 t0 = time.time()
 train_labels_onehot = train_labels.to_numpy()
 seqs = list(train_values.sequence)
 enc_seqs = pad_dna(seqs, max_seq_length)
 enc_seqs = append_rc(enc_seqs, cnn_filter_length)
 enc_seqs = convert_onehot_4(enc_seqs)
+print('One hot encoding finished')
 
 X_train, X_test, y_train, y_test = train_test_split(enc_seqs, train_labels_onehot, test_size=0.10, random_state=12)
 X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.70, random_state=12)
